4637e47b692abaf33803322778fedf126fffc86d
Cheng Yang <chengyang.ong@syntrinos.com.my>
2021-06-28 00:26:22 +0800

    Update service worker

---
+++ script.ts
@@ -0,0 +1,13 @@
+export default class Main {
+    constructor() {
+      if ("serviceWorker" in navigator) {
+        navigator.serviceWorker
+          .register("/service-worker.js", { scope: "/" })
+          .then(function () {
+            console.log("Service Worker Registered");
+          });
+      }
+    }
+  }
+  
+  new Main();
