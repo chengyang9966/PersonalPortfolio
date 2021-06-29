ffaf8df25143821351c99363460595ef1ee4852b
Cheng Yang <chengyang.ong@syntrinos.com.my>
2021-06-30 02:39:52 +0800

    responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,89 @@
+import Header from "src/components/Header";
+import Body from "src/components/Body";
+import Personal from "src/components/Personal";
+import ImageContainer from "src/components/ImageContainer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ContactMe from "src/components/ContactMe";
+import FloatButton from "src/components/FloatButton";
+import Loading from "src/components/Loading";
+import PreviewImage from "src/components/previewImage";
+import { Mode } from "src/Types";
+import { useState, useEffect } from "react";
 
-import Header from 'src/components/Header';
-import Body from 'src/components/Body';
-import Personal from 'src/components/Personal';
-import ImageContainer from 'src/components/ImageContainer';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import ContactMe from 'src/components/ContactMe';
-import FloatButton from 'src/components/FloatButton';
-import Loading from 'src/components/Loading';
-import PreviewImage from 'src/components/previewImage';
-import {Mode} from 'src/Types'
-import {useState,useEffect} from 'react';
 function App() {
-  const [loading,setLoading]=useState<Boolean>(true)
-  var root = document.getElementById('root');
-  useEffect(()=>{
-    if(root){
-      setLoading(false)
-    }else{
-      setLoading(true)
+  const [loading, setLoading] = useState<Boolean>(true);
+  var root = document.getElementById("root");
+  useEffect(() => {
+    if (root) {
+      setLoading(false);
+    } else {
+      setLoading(true);
     }
-  },[root])
+  }, [root]);
 
   var body = document.body;
   var html = document.documentElement;
-  const [theme,setTheme]=useState(Mode.light)
-    
+  const [theme, setTheme] = useState(Mode.light);
+
   var height = 0;
   var h = 0;
-  
+
   var initiateHeights = function () {
-    height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
-    h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+    height = Math.max(
+      body.scrollHeight,
+      body.offsetHeight,
+      html.clientHeight,
+      html.scrollHeight,
+      html.offsetHeight
+    );
+    h =
+      window.innerHeight ||
+      document.documentElement.clientHeight ||
+      document.body.clientHeight;
     console.log("heights were initialised:", height, h);
-  }
-  
+  };
+
   initiateHeights();
-  
-  var resize = function (e:any) {
-    var scrolled = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
-    height > 0 ? e[0].style.width = scrolled/(height-h) * 100 + "%" : e.style.width = 0 + "%";
-  }
-  
+
+  var resize = function (e: any) {
+    var scrolled = Math.max(
+      document.body.scrollTop,
+      document.documentElement.scrollTop
+    );
+    height > 0
+      ? (e[0].style.width = (scrolled / (height - h)) * 100 + "%")
+      : (e.style.width = 0 + "%");
+  };
+
   document.onscroll = function () {
     resize(document.getElementsByClassName("headerKey"));
   };
-  
+
   window.onresize = function () {
     initiateHeights();
-  }
+  };
 
+  return loading ? (
+    <Loading />
+  ) : (
+    <>
+      <div className={theme} style={{ marginBottom: "80rem" }}>
+        <Header theme={setTheme} />
+        <span className="headerKey" />
+     
+        <Body />
 
-  return (
-    loading?<Loading/>
-    :<>
-  <div className={theme} style={{marginBottom:'80rem'}}>
-      <Header theme={setTheme}/>
-      <span className="headerKey"/>
-      <Body/>
-
-      <Personal/>
-      <PreviewImage/>
-      <ImageContainer/>
-      <ContactMe/>
-    </div>
-    <FloatButton/>
-    <div style={{display:'flex',justifyContent:'center'}}>
-      <p style={{  marginBottom: 0}}>Copyright © 2021 Cheng Yang.  All rights reserved.  </p>
-    </div>
+        <Personal />
+        <PreviewImage />
+        <ImageContainer />
+        <ContactMe />
+      </div>
+      <FloatButton />
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <p style={{ marginBottom: 0 }}>
+          Copyright © 2021 Cheng Yang. All rights reserved.{" "}
+        </p>
+      </div>
     </>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,67 +1,113 @@
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
-import React, { useState,useEffect } from "react";
-import {BsMoon,BsSun} from "react-icons/all";
-import {Mode,HeaderType} from 'src/Types'
+import React, { useState, useEffect } from "react";
+import { BsMoon, BsSun } from "react-icons/all";
+import { Mode, HeaderType } from "src/Types";
 
-
-const Header = (props:HeaderType) => {
-  const [theme,setTheme]=useState(Mode.light);
-  useEffect(()=>{
-    var temp =localStorage.getItem('theme')
-    if(temp){
-      setTheme(temp)
+const Header = (props: HeaderType) => {
+  const [theme, setTheme] = useState(Mode.light);
+  useEffect(() => {
+    var temp = localStorage.getItem("theme");
+    if (temp) {
+      setTheme(temp);
       document.body.className = "";
-      document.body?.classList.add(theme)
+      document.body?.classList.add(theme);
     }
-  },[])
+  }, []);
 
-  useEffect(()=>{
-    const intervalId = setInterval(
-      ()=>{
-        props.theme(theme)
-        localStorage.setItem('theme',theme)
-        document.body.className = "";
-        document.body?.classList.add(theme)
-      },2000
-    )
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      props.theme(theme);
+      localStorage.setItem("theme", theme);
+      document.body.className = "";
+      document.body?.classList.add(theme);
+    }, 2000);
     return () => clearInterval(intervalId); //This is important
-  },[theme])
+  }, [theme]);
 
   return (
     <>
-    <Navbar  fixed="top" expand="lg">
-      <Container >
-        <Navbar.Brand href="#home">Cheng Yang</Navbar.Brand>
-         <Navbar.Collapse id="responsive-navbar-nav">
-         <Container>
-          <Nav className="me-auto ">
-            <Nav.Item>
-              <Nav.Link href="#home">Home</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="#about">About</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="#skill">Skill</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="#contact">Contact</Nav.Link>
-            </Nav.Item>
-          </Nav>
-         </Container>
-         <Navbar.Text onClick={()=>setTheme(theme===Mode.dark?Mode.light:Mode.dark)}>
-        { theme===Mode.light?<BsSun className='sun'/>:<BsMoon className='moon' />}
-         </Navbar.Text>
+      {/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand" href="#home">Cheng Yang</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href="#home">Home <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#about">About</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#skill">Skill</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#portfolio">Portfolio</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#contact">Contact</a>
+      </li>
+      <li className="nav-item my-2 my-lg-0"  onClick={() =>
+                setTheme(theme === Mode.dark ? Mode.light : Mode.dark)
+              }>
+                {theme === Mode.light ? (
+                <BsSun className="sun" />
+              ) : (
+                <BsMoon className="moon" />
+              )}
+      </li>
+    </ul>
+
+  </div>
+</nav> */}
+      <Navbar fixed="top" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Cheng Yang</Navbar.Brand>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <Navbar.Collapse id="navbarSupportedContent">
+            <Container>
+              <Nav className="me-auto ">
+                <Nav.Item>
+                  <Nav.Link href="#home">Home</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link href="#about">About</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link href="#skill">Skill</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link href="#contact">Contact</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Container>
+            <Navbar.Text>
+              {theme === Mode.light ? (
+                <BsSun className="sun" />
+              ) : (
+                <BsMoon className="moon" />
+              )}
+            </Navbar.Text>
           </Navbar.Collapse>
-          
-      </Container>
-    </Navbar>
-    
+        </Container>
+      </Navbar>
     </>
   );
 };
